9d8fe1757a10314bf33fb9ebd12be22fc4db7dbd
yjchess <yangjian.zhou@live.co.uk>
2017-08-14 12:28:13 +0100

    Update d3HistogramCharting.js

--- bar_chart/d3HistogramCharting.js
+++ bar_chart/d3HistogramCharting.js
@@ -1,32 +1,33 @@
 var makeHistogramChart = function(data) {
     //start charting once we have collected all the data
-    console.log("data array size: " + data.length);
-    var allIssuesList=[], allIssuesCount=[] ,loopId=-1;
-    
-    var data4Histogram=[];    
+    //console.log("data array size: " + data.length);
+    //find what values are in name property inside label objects
     var fullLabelNameList=[];
     fullLabelNameList.push("bug");
     data.forEach(x=>{ x.issues.forEach(y=>{
-            if(y.labels.length>0){
-                y.labels.forEach(z=>{
-                    var pushIt=true;
-                    fullLabelNameList.forEach(t=>{
-                        if(t==z.name){pushIt=false;}
-                    })
-                    if(pushIt){fullLabelNameList.push(z.name);}
+        if(y.labels.length>0){
+            y.labels.forEach(z=>{
+                var pushIt=true;
+                fullLabelNameList.forEach(t=>{
+                    if(t==z.name){pushIt=false;}
                 })
-            }
+                if(pushIt){fullLabelNameList.push(z.name);}
+            })
+        }
     })})
-
-    var fullLabelList=["bug","duplicate","enhancement","help wanted",
-        "invalid","question","wontfix","projectId","totalIssue","valuedLabel"];
+    // define what name values are required to be counted and plot in the histogram
+    //var fullLabelList=["bug","enhancement","question","projectId","totalIssue","name"];
+    var fullLabelList=["bug","enhancement","duplicate","help wanted",
+        "invalid","question","wontfix","projectId","totalIssue","name"];
+    // get the required data into data4Histogram array
+    var data4Histogram=[], loopId=-1;    
     data.forEach(x=>{ var issueCount=0;  loopId++;
         var dataItem={};
         fullLabelList.forEach(q=>{ dataItem[q]=0;});
+        dataItem["name"]=x.repo.name;
         dataItem["projectId"]= loopId;                 
         x.issues.forEach(t=>{ 
-            issueCount++; t.Id=loopId; allIssuesList.push(t);
-            var labelCount=t.labels.length;
+            issueCount++; t.Id=loopId;
             if(t.labels.length>0){
                 t.labels.forEach(p=>{
                     fullLabelList.forEach(q=>{
@@ -35,64 +36,156 @@ var makeHistogramChart = function(data) {
                 })                
             }
         }) 
-        allIssuesCount.push({Id:loopId,totalIssue:issueCount});
-        dataItem["totalIssue"]= issueCount;                 
+        dataItem["totalIssue"]= issueCount; 
+        if(issueCount>0){
+            fullLabelList.forEach(t=>{
+                if(t != "projectId" && t != "totalIssue" && t != "name"){
+                    dataItem[t]=Math.round(dataItem[t]/issueCount*100);
+                }
+            })
+        }                
         data4Histogram.push(dataItem);
-        //data4Histogram.filter(p=>p.projectId==loopId)
-        //.forEach(q=>q.totalIssue=issueCount);
     })
+
+    var data4HistogramChart=[];
+    fullLabelList.forEach(t=>{
+        if(t != "projectId" && t != "totalIssue" && t !="name"){
+            data4Histogram.forEach(p=>{
+                var label="", quantity=0, projectId=0;
+                label=t; quantity=Math.round(p[t]); projectId=p["projectId"];
+                data4HistogramChart.push({
+                    "label":label, "quantity":quantity, "projectId":projectId
+                })
+            })
+        }
+    })
+
+    var data4GroupedBarChart=[],columns=[],projId=0;
+    columns[0]="label";
+    data.forEach(x=>{ columns[projId+1]=x.repo.name; projId++;});
+    var data4GroupedBarChart=[];
+    var iLabel=0;
+    data4GroupedBarChart['columns']=columns;
+    fullLabelList.forEach(t=>{
+        if(t != "projectId" && t != "totalIssue" && t != "name"){
+            var dataItem=[];
+            dataItem["label"]=t;
+            for (var ii=1; ii<columns.length;ii++) {
+                dataItem[columns[ii]]=1;
+            }
+            data4HistogramChart.forEach(p=>{
+                if(t==p.label){
+                    dataItem["label"]=p.label;
+                    dataItem[columns[p.projectId+1]]=p.quantity==0?1:p.quantity;
+                }
+            })
+            data4GroupedBarChart.push(dataItem);
+        }
+    })
+
     var check=true;
 
-    var dataSample = [{"food":"bug","quantity":84}
-    ,{"food":"duplicate","quantity":35}
-    ,{"food":"enhancement","quantity":62}
-    ,{"food":"help wanted","quantity":92}
-    ,{"food":"invalid","quantity":30}
-    ,{"food":"question","quantity":21}
-    ,{"food":"wontfix","quantity":13}];
-
-    var dataArray = [23, 13, 21, 14, 37, 15, 18, 34, 30];
-
-    //plotSampleBarChart(dataArray);
-    plotSampleBarChartv3(dataSample);
+    plotHistogramBarChart(data4HistogramChart);
+    plotGroupedBarChart(data4GroupedBarChart);
 }
 
+var plotGroupedBarChart=function(data){
+    var svg = d3.select("#groupedBarChart").append("svg").attr("width",700).attr("height",500);
+    margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var plotSampleBarChart=function(data){
-    // Create variable for the SVG
-var svg = d3.select("#barChartID").append("svg")
-          .attr("height","100%")
-          .attr("width","100%");
+    var x0 = d3.scaleBand()
+        .rangeRound([0, width])
+        .paddingInner(0.1);
 
-// Select, append to SVG, and add attributes to rectangles for bar chart
-svg.selectAll("rect")
-    .data(data)
-    .enter().append("rect")
-          .attr("class", "bar")
-          .attr("height", function(d, i) {return (d * 10)})
-          .attr("width","40")
-          .attr("x", function(d, i) {return (i * 60) + 25})
-          .attr("y", function(d, i) {return 400 - (d * 10)});
+    var x1 = d3.scaleBand()
+        .padding(0.05);
 
-// Select, append to SVG, and add attributes to text
-svg.selectAll("text")
-    .data(dataArray)
-    .enter().append("text")
-    .text(function(d) {return d})
-           .attr("class", "text")
-           .attr("x", function(d, i) {return (i * 60) + 36})
-           .attr("y", function(d, i) {return 415 - (d * 10)});
+    var y = d3.scaleLinear()
+        .rangeRound([height, 0]);
+
+    var z = d3.scaleOrdinal()
+        //.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c"]);
+    /*
+    d3.csv("./groupedBarChartData.csv", function(d, i, columns) {
+    for (var i = 1, n = columns.length; i < n; ++i) d[columns[i]] = +d[columns[i]];
+    return d;
+    }, function(error, data) {
+    if (error) throw error;
+    });
+    */
+    var keys = data.columns.slice(1);
+    var extraYPercentage=10;
+    x0.domain(data.map(function(d) { return d.label; }));
+    x1.domain(keys).rangeRound([0, x0.bandwidth()]);
+    y.domain([0, d3.max(data, function(d) { 
+        return d3.max(keys, function(key) { return d[key]; })+extraYPercentage; })]).nice();
+
+    g.append("g")
+        .selectAll("g")
+        .data(data)
+        .enter().append("g")
+        .attr("transform", function(d) { return "translate(" + x0(d.label) + ",0)"; })
+        .selectAll("rect")
+        .data(function(d) { return keys.map(function(key) { return {key: key, value: d[key]}; }); })
+        .enter().append("rect")
+        .attr("x", function(d) { return x1(d.key); })
+        .attr("y", function(d) { return y(d.value); })
+        .attr("width", x1.bandwidth())
+        .attr("height", function(d) { return height - y(d.value); })
+        .attr("fill", function(d) { return z(d.key); });
+
+    g.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x0));
+
+    g.append("g")
+        .attr("class", "axis")
+        .call(d3.axisLeft(y).ticks(null, "s"))
+        .append("text")
+        .attr("x", 2)
+        .attr("y", y(y.ticks().pop()) + 0.5)
+        .attr("dy", "0.32em")
+        .attr("fill", "#000")
+        .attr("font-weight", "bold")
+        .attr("text-anchor", "start")
+        .text("Percentage");
+
+    var legend = g.append("g")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 10)
+        .attr("text-anchor", "end")
+        .selectAll("g")
+        .data(keys.slice().reverse())
+        .enter().append("g")
+        .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+    legend.append("rect")
+        .attr("x", width - 19)
+        .attr("width", 19)
+        .attr("height", 19)
+        .attr("fill", z);
+
+    legend.append("text")
+        .attr("x", width - 24)
+        .attr("y", 9.5)
+        .attr("dy", "0.32em")
+        .text(function(d) { return d; });
 }
 
-
-var plotSampleBarChartv3=function(data){
+var plotHistogramBarChart=function(data){
     var margin = {top:10, right:10, bottom:90, left:50};
     var width = 660 - margin.left - margin.right;
     var height = 300 - margin.top - margin.bottom;
     var color = d3.scaleOrdinal().range(d3.schemeCategory20);
+    var extraYPercentage=10;
     //var formatPercent = d3.format(".0%");
     
-    var xScale = d3.scaleBand().rangeRound([0, width]).padding(0.03);
+    var xScale = d3.scaleBand().range([0, width]).padding(0.03);
     //d3.scaleOrdinal().range([0, width]).round( .03)
     var yScale = d3.scaleLinear().range([height, 0]);
 
@@ -100,7 +193,6 @@ var plotSampleBarChartv3=function(data){
         
     //var yAxis = d3.svg.axis().scale(yScale).orient("left");
 
-    $("#testID").text("This is a test!");
 
     var svgContainer = d3.select("#barChartID").append("svg")
             .attr("width", width+margin.left + margin.right)
@@ -108,9 +200,9 @@ var plotSampleBarChartv3=function(data){
             .append("g").attr("class", "container")
             .attr("transform", "translate("+ margin.left +","+ margin.top +")");
 
-    xScale.domain(data.map(function(d) { return d.food; }));
-    //yScale.domain([0, d3.max(data, function(d) { return d.quantity; })]);
-    yScale.domain([0, 110]);
+    xScale.domain(data.map(function(d) { return d.label+d.projectId; }));
+    yScale.domain([0, d3.max(data, function(d) { return d.quantity; })+extraYPercentage]);
+    //yScale.domain([0, 110]);
 
     var rangeBand=function(){ return (width+margin.left + margin.right);}
     xScale.rangeBand=rangeBand;
@@ -125,12 +217,7 @@ var plotSampleBarChartv3=function(data){
                 .attr("dx", "-.8em")
                 .attr("dy", ".15em")
                 .attr("transform", "rotate(-35)");
-    /*
-    vis.selectAll(".xaxis text")  // select all the text elements for the xaxis
-          .attr("transform", function(d) {
-              return "translate(" + this.getBBox().height*-2 + "," + this.getBBox().height + ")rotate(-45)";
-        });        
-    */
+
     // Uncomment this block if you want the y axis
     var yAxis_g = svgContainer.append("g")
             .attr("class", "y axis")
@@ -150,27 +237,28 @@ var plotSampleBarChartv3=function(data){
             .enter()
             .append("rect")
             .attr("class", "bar")
-            .attr("x", function(d) { return xScale(d.food); })
-//            .attr("width", width+margin.left + margin.right)
+            .attr("x", function(d) { return xScale(d.label+d.projectId); })
             .attr("width", xScale.bandwidth())
             .attr("y", function(d) { return yScale(d.quantity); })
             .attr("height", function(d) { return height - yScale(d.quantity); });
 
 
 // Controls the text labels at the top of each bar. Partially repeated in the resize() function below for responsiveness.
-	svgContainer.selectAll(".text")  		
+    /*
+    svgContainer.selectAll(".text")  		
 	  .data(data)
 	  .enter()
 	  .append("text")
 	  .attr("class","label")
-	  .attr("x", (function(d) { return xScale(d.food) + xScale.bandwidth() / 2 ; }  ))
+	  .attr("x", (function(d) { return xScale(d.label+d.projectId) + xScale.bandwidth() / 2 ; }  ))
 	  .attr("y", function(d) { return yScale(d.quantity) + 1; })
 	  .attr("dy", ".75em")
       .text(function(d) { return d.quantity; });  
-        
+       
 
       document.addEventListener("DOMContentLoaded", resize);
       d3.select(window).on('resize', resize); 
+      */
 }
 
 function resize() {
@@ -196,12 +284,12 @@ function resize() {
         .style('width', (width + margin.left + margin.right) + 'px');
 
     svgContainer.selectAll('.bar')
-    	.attr("x", function(d) { return xScale(d.food); })
+    	.attr("x", function(d) { return xScale(d.label); })
       .attr("width", xScale.rangeBand());
       
    svgContainer.selectAll("text")  		
-	 // .attr("x", function(d) { return xScale(d.food); })
-	 .attr("x", (function(d) { return xScale(d.food	) + xScale.rangeBand() / 2 ; }  ))
+	 // .attr("x", function(d) { return xScale(d.label); })
+	 .attr("x", (function(d) { return xScale(d.label	) + xScale.rangeBand() / 2 ; }  ))
       .attr("y", function(d) { return yScale(d.quantity) + 1; })
       .attr("dy", ".75em");   	      
 
